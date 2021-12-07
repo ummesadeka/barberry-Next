07d222e681f777f4c21cd95c3abcd5671aae7a85
ummesadeka <sadekaahmed53@gmail.com>
2021-12-07 14:41:32 +0600

    Creating List Product

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,37 @@
 
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@material-ui/core'
 import Layout from '../components/Layout'
+import data from '../utils/data'
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>products</h1>
-        <ul>
-          <li>Product</li>
-          <li>Product</li>
-          <li>Product</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                  component="img"
+                  image={product.image}
+                  title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>$ {product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   )
---
+++ utils/data.js
@@ -0,0 +1,104 @@
+const data ={
+    products: [
+        {
+            name:'College Bag',
+            category: 'bags',
+            image: '/images/bag1.jpg',
+            price: 70,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        },
+        {
+            name:'Hand Bag',
+            category: 'bags',
+            image: '/images/bag2.jpg',
+            price: 80,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        },
+        {
+            name:'Hand Bag',
+            category: 'bags',
+            image: '/images/bag3.jpg',
+            price: 90,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        },
+        {
+            name:'Hand Bag',
+            category: 'bags',
+            image: '/images/bag4.jpg',
+            price: 60,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        },
+        {
+            name:'Hand Bag',
+            category: 'bags',
+            image: '/images/bag5.jpg',
+            price: 70,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        },
+        {
+            name:'Hand Bag',
+            category: 'bags',
+            image: '/images/bag6.jpg',
+            price: 70,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        },
+        {
+            name:'Small Bag',
+            category: 'bags',
+            image: '/images/bag7.jpg',
+            price: 40,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        },
+        {
+            name:'Bag Set',
+            category: 'bags',
+            image: '/images/bag8.jpg',
+            price: 190,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        },
+        {
+            name:'Hand Bag',
+            category: 'bags',
+            image: '/images/bag9.jpg',
+            price: 70,
+            brand: 'Gucci',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Popular Bag'
+        }
+    ]
+};
+export default data;
